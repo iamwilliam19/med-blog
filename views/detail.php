67f3bfad8216154e7d4fbca68adc44ebfe8754e0
william <williamikeji@gmail.com>
2019-11-05 17:25:08 +0100

    blog details page completed

---
+++ views/detail.php
@@ -0,0 +1,122 @@
+<section class="blogBox">
+    <div class="blogLeft">
+        <div class="blogProImage">
+            <img src="images/sample.jpg" />
+        </div>
+        <div class="proName ">Ikeji Chukwunonso</div>
+        <div class="proDescription">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et, voluptate nisi! Ab dolorum con
+        </div>
+        <div class="proEditLink">
+            <a href="detail">Edit Profile</a>
+        </div>
+        
+            <div class="firstBearer">
+                <ul>
+                    
+                    <a href="detail"><li><strong>My Posts</strong></li></a>
+                    <a href="detail"><li><strong>Recent Posts</strong></li></a>
+                    <li class="except "><strong>Post Fields</strong><i class="ui dropdown icon pushRight"></i></li>
+                    
+                </ul>
+            </div>
+
+            
+    
+
+            <div class="secondBearer">
+                <ul>
+                   <a href="detail"> <li><strong>All</strong></li></a>
+                   <a href="detail"><li><strong>Anatomy</strong></li></a>
+                   <a href="detail"><li><strong>Pharmacology</strong></li></a>
+                   <a href="detail"><li><strong>Physiology</strong></li></a>
+                   <a href="detail"><li><strong>Medical Biochemistry</strong></li></a>
+                </ul>
+            </div>
+
+            <br />
+            <i class="ui like icon"></i> 120 
+            <i class=" ui comment icon" id="padIcon"></i> 129
+
+            <a href="detail"> <div class="ui button" id="newBut"> New Post  </div></a>
+        
+    </div>
+
+    <!--right blog page -->
+
+    <div class="blogRight">
+        <div class="ui text container" id="topPad">
+        
+            <div class="ui header"> What causes cancer that is unknown to man</div>
+            <div class="customMeta">By Ikeji William | 04 April 2019</div>
+            <div class="customMeta">Post Field: Medical Biochemistry</div>
+
+            <div class="ui image" id="imageBase">
+               
+                <img src="images/js.jpg" alt="" />
+            </div>
+
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum unde non fugiat corporis cumque, iusto impedit ipsa illo voluptas ratione dicta eaque, quidem praesentium maxime eveniet. Explicabo, quod? Vel, laudantium?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo culpa expedita eligendi eaque cumque ab quidem similique sint! Accusamus autem architecto nam, in veritatis culpa maiores atque doloremque id iste.
+            </p>
+
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum unde non fugiat corporis cumque, iusto impedit ipsa illo voluptas ratione dicta eaque, quidem praesentium maxime eveniet. Explicabo, quod? Vel, laudantium?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo culpa expedita eligendi eaque cumque ab quidem similique sint! Accusamus autem architecto nam, in veritatis culpa maiores atque doloremque id iste.
+            </p>
+            <i class="ui like icon"></i> 120 
+            <i class=" ui comment icon" id="padIcon"></i> 129
+
+            <div class="ui small header">Comments</div>
+
+            <!--comments start-->
+            <div class="ui comments">
+                <!--main comment starts-->
+                <div class="comment">
+                    <div class=" avatar" id="avatar">
+                        <img src="images/js.jpg" alt="" />
+                    </div>
+
+                    <div class="content">
+                        <a class="author">William Ikeji</a>
+                        <div class="metadata">1 hr ago</div>
+                        <div class="text"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi asperiores doloribus unde quod omnis, facere quo quaerat voluptatibus. Maiores harum quo dolores ab sit ipsam maxime, quas odit veritatis aliquam. </div>
+                         <div class="actions">   
+                            <i class="ui reply icon"></i> 120
+                            <i class="ui like icon" id="padIcon"></i> 2
+                        </div>
+                    </div>
+
+                    <!-- sub comment start-->
+                    <div class="comments">
+                                <!--one comment end-->
+                        <div class="comment">
+                            <div class=" avatar">
+                                <img src="images/js.jpg" alt="" />
+                            </div>
+
+                            <div class="content">
+                                <a class="author">William Ikeji</a>
+                                <div class="metadata">1 hr ago</div>
+                                <div class="text"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi asperiores doloribus unde quod omnis, facere quo quaerat voluptatibus. Maiores harum quo dolores ab sit ipsam maxime, quas odit veritatis aliquam. </div>
+                                <div class="actions">   
+                                    <i class="ui reply icon"></i> 120
+                                    <i class="ui like icon" id="padIcon"></i> 2
+                                </div>
+                            </div>
+                        </div>
+                        <!--one comment end-->
+                    </div>
+                    <!-- sub comments end-->
+                </div>
+                <!--main comment end-->
+            </div>
+            <!--comments end-->
+
+        </div>
+    </div>
+
+    <div style="clear:both"></div>
+</section>
+
